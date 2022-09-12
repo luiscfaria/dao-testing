23849cbd0163c7e07f38f7af1b298333f317e177
Luis <luiscarlossfaria@gmail.com>
2022-09-12 18:12:52 -0300

    Initial commit - added smart contracts

---
+++ smart-contracts/constants.js
@@ -0,0 +1,5 @@
+// Replace the value with your NFT contract address
+const CRYPTODEVS_NFT_CONTRACT_ADDRESS =
+  "0x01f2707c796cEA0cF61f1e639134b69AB41a301B";
+
+module.exports = { CRYPTODEVS_NFT_CONTRACT_ADDRESS };
